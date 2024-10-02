146c0b3ce563762e577632e94e99d88764640278
Malini04 <101280431+Malini04@users.noreply.github.com>
2024-10-02 16:02:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Fetchdata from './Fetchdata';
+
+function App() {
+  return (
+    <div className="App">
+      <Fetchdata />
+    </div>
+  );
+}
+
+export default App;
